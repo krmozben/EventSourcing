c7f70346111846b6393eb4eb8e758fab6cb69b31
kerim_ozben <kerim.ozben@consultant.morhipo.com>
2022-04-12 14:36:24 +0300

    Add project files.

---
+++ EventSourcing.API/BackgroundServices/ProductReadModelEventStore.cs
@@ -0,0 +1,110 @@
+﻿using EventSourcing.API.EventStores;
+using EventSourcing.API.Models;
+using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using System.Text.Json;
+
+namespace EventSourcing.API.BackgroundServices
+{
+    public class ProductReadModelEventStore : BackgroundService
+    {
+        private readonly IEventStoreConnection _eventStoreConnection;
+        private readonly ILogger<ProductReadModelEventStore> _logger;
+        private readonly IServiceProvider _serviceProvider;
+
+        public ProductReadModelEventStore(IEventStoreConnection eventStoreConnection, ILogger<ProductReadModelEventStore> logger, IServiceProvider serviceProvider)
+        {
+            _eventStoreConnection = eventStoreConnection;
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _eventStoreConnection.ConnectToPersistentSubscriptionAsync(ProductStream.StreamName, ProductStream.GroupName, EventAppeared, autoAck: false);
+
+            ///autoAck true: EventAppeared metodunda exception fırlamadı ise EventStore event i gönderildi sayar. Exception fırlarsa ilgili event i Event Store tekrar gönderecektir.
+
+            ///autoAck false: Bu event başarılı şekilde alıp işlediğimizi Event Store a biz bildirmeliyiz. 
+            ///ÖRN : arg1.Acknowledge(arg2.Event.EventId);
+
+            ///Örnek olması açısından burada false vererek Event Store başarılı bir şekilde aldığımız biz bildiriyoruz. Yani otomatik kullanımdan manuel kullanıma geçiyoruz :)
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+
+        private async Task EventAppeared(EventStorePersistentSubscriptionBase arg1, ResolvedEvent arg2)
+        {
+            var type = Type.GetType($"{Encoding.UTF8.GetString(arg2.Event.Metadata)}, EventSourcing.Shared");
+
+            _logger.LogInformation($"The mesaage processing ...{type}");
+
+            var eventData = Encoding.UTF8.GetString(arg2.Event.Data);
+
+            var @event = JsonSerializer.Deserialize(eventData, type);
+
+            using var scope = _serviceProvider.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            Product product = null;
+
+            switch (@event)
+            {
+                case ProductCreatedEvent productCreatedEvent:
+
+                    product = new Product
+                    {
+                        Name = productCreatedEvent.Name,
+                        Id = productCreatedEvent.Id,
+                        Price = productCreatedEvent.Price,
+                        Stock = productCreatedEvent.Stock,
+                        UserId = productCreatedEvent.UserId,
+                    };
+                    context.Products.Add(product);
+                    break;
+
+                case ProductNameChangedEvent productNameChangedEvent:
+
+                    product = context.Products.Find(productNameChangedEvent.Id);
+                    if (product != null)
+                    {
+                        product.Name = productNameChangedEvent.ChangedName;
+                    }
+                    break;
+
+                case ProductPriceChangedEvent productPriceChangedEvent:
+
+                    product = context.Products.Find(productPriceChangedEvent.Id);
+                    if (product != null)
+                    {
+                        product.Price = productPriceChangedEvent.ChangedPrice;
+                    }
+                    break;
+
+                case ProductDeletedEvent productDeletedEvent:
+
+                    product = context.Products.Find(productDeletedEvent.Id);
+                    if (product != null)
+                    {
+                        context.Products.Remove(product);
+                    }
+                    break;
+            }
+
+            await context.SaveChangesAsync();
+
+            ///27. satırda açıklaması mevcut
+            arg1.Acknowledge(arg2.Event.EventId);
+        }
+    }
+}
---
+++ EventSourcing.API/Queries/GetProductAllListByUserIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using EventSourcing.API.DTOs;
+using MediatR;
+
+namespace EventSourcing.API.Queries
+{
+    public class GetProductAllListByUserIdQuery : IRequest<List<ProductDto>>
+    {
+        public int UserId { get; set; }
+    }
+}
